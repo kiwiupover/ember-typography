a7fdfa013bc77d2ec6f12578704a4d48e35a75cf
Dave Laird <kiwiupover@gmail.com>
2017-11-29 11:58:20 -0800

    Load font async

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = {
   name: 'ember-typography',
   contentFor: function (type, config) {
     if (type === 'body-footer') {
-      return `<link rel="stylesheet" rel="preconnect" type="text/css" href="https://cloud.typography.com/${config.typography.CSS_KEY}/css/fonts.css" />`;
+      return `<link rel="stylesheet" rel="preconnect" type="text/css" href="https://cloud.typography.com/${config.typography.CSS_KEY}/css/fonts.css" async>`;
     }
   }
 };
